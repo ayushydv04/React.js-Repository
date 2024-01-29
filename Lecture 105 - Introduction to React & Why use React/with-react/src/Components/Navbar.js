43415fb2e3b9ae81efebc0614ea12097655a73a3
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-29 11:43:09 +0530

    commit 29/1/24

---
+++ Lecture 105 - Introduction to React & Why use React/with-react/src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from './Footer'
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact Us</li>
+        <Footer/>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
