2f468c4758b06f677ec21e4e2fc19888977cacbb
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-01-31 00:05:09 +0530

    commit 30/1/24

--- 02. Components, Props and JSX in React/first-app/src/App.jsx
+++ 02. Components, Props and JSX in React/first-app/src/App.jsx
@@ -12,11 +12,15 @@ function App() {
       <div>
         <Navbar />
       </div>
-      <div className="Cards">
-        <Card title = "Card 1" description = " Card 1 desc" />
-        <Card title = "Card 2" description = " Card 2 desc" />
-        <Card title = "Card 3" description = " Card 3 desc" />
-        <Card title = "Card 4" description = " Card 4 desc" />
+      <div className="main">
+        <div className="Cards">
+          <Card title="Card 1" description=" Card 1 desc" />
+          <Card title="Card 2" description=" Card 2 desc" />
+          <Card title="Card 3" description=" Card 3 desc" />
+          <Card title="Card 4" description=" Card 4 desc" />
+
+        </div>
+
       </div>
       <div>
         <Footer />
--- 02. Components, Props and JSX in React/first-app/src/Components/Card.jsx
+++ 02. Components, Props and JSX in React/first-app/src/Components/Card.jsx
@@ -4,7 +4,7 @@ import "./Card.css"
 const Card = (props) => {
   return (
     <div>
-      <div className="cards">
+      <div className="cards" style={{overflow: 'hidden', }}>
         <img src="https://cdn.shopify.com/s/files/1/0306/6419/6141/articles/coding_languages.png?v=1619126283" alt="" width="260" style={{}}/>
         <h1>{props.title}</h1>
         <p>{props.description}</p>
