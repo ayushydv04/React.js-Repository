27879a66f494431dcbf4901ceaa2c826b17de1ea
Ayush Kumar Yadav <113637305+ayushydv04@users.noreply.github.com>
2024-02-20 20:37:29 +0530

    commit 20/2/24

---
+++ 08. React Router- Routing in React/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// Well replace anchor tag with Links of 
+
+const Navbar = () => {
+  return (
+    <nav>
+        <a href="/"><li>Home</li></a>
+        <a href="/about"><li>About</li></a>
+        <a href="/login"><li>Login</li></a>
+    </nav>
+  )
+}
+
+export default Navbar
